5d7a5e5ea5656061a7545719879fc09d21d1c98c
Mikhail Dubov <mikhail@alphasights.com>
2015-01-23 18:12:49 +0000

    initial setup

---
+++ sudoku.go
@@ -0,0 +1,9 @@
+package sudoku
+
+import (
+  "fmt"
+)
+
+func main() {
+  fmt.Println("Hello world")
+}
---
+++ sudoku_test.go
@@ -0,0 +1,5 @@
+package sudoku //same package name as source file
+
+import (
+    "testing" //import go package for testing related functionality
+    )
