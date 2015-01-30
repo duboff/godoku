a8c1c280a8f2872dda07681a58962a2e1e64985c
Mikhail Dubov <mikhail@alphasights.com>
2015-01-30 18:16:38 +0000

    Get to units

--- sudoku.go
+++ sudoku.go
@@ -1,9 +1,46 @@
 package sudoku
 
-import (
-  "fmt"
-)
+import ()
+
+func Cross(A, B string) []string {
+	resp := make([]string, len(A)*len(B))
+
+	ind := 0
+	for i := 0; i < len(A); i++ {
+		for j := 0; j < len(B); j++ {
+			resp[ind] = string(A[i]) + string(B[j])
+			ind++
+		}
+	}
+	return resp
+}
+
+func UnitList(rows, cols string) [][]string {
+	rowThrees := []string{"ABC", "DEF", "GHI"}
+	colThrees := []string{"123", "456", "789"}
+
+	resp := make([][]string, 27)
+
+	ind := 0
+
+	for _, c := range cols {
+		resp[ind] = Cross(rows, string(c))
+		ind++
+	}
+
+	for _, r := range rows {
+		resp[ind] = Cross(string(r), cols)
+		ind++
+	}
+
+	for _, r := range rowThrees {
+		for _, c := range colThrees {
+			resp[ind] = Cross(r, c)
+			ind++
+		}
+	}
+	return resp
+}
 
 func main() {
-  fmt.Println("Hello world")
 }
--- sudoku_test.go
+++ sudoku_test.go
@@ -1,5 +1,24 @@
-package sudoku //same package name as source file
+package sudoku
 
 import (
-    "testing" //import go package for testing related functionality
-    )
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestCross(t *testing.T) {
+	testCross := Cross("abc", "12")
+	expectedCross := []string{"a1", "a2", "b1", "b2", "c1", "c2"}
+
+	assert.Equal(t, 6, len(testCross))
+	assert.Equal(t, expectedCross, testCross)
+}
+
+func TestUnitList(t *testing.T) {
+	unitList := UnitList("ABCDEFGHI", "123456789")
+	firstRow := []string{"A1", "B1", "C1", "D1", "E1", "F1", "G1", "H1", "I1"}
+	lastBox := []string{"G7", "G8", "G9", "H7", "H8", "H9", "I7", "I8", "I9"}
+
+	assert.Equal(t, 27, len(unitList))
+	assert.Equal(t, firstRow, unitList[0])
+	assert.Equal(t, lastBox, unitList[26])
+}
